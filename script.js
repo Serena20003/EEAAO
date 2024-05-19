8ff429df26940f0f4f1b28fb54a3d9d61ffa90c0
Serena20003 <scli@usc.edu>
2024-05-19 17:25:15 +0800

    Changed links

--- script.js
+++ script.js
@@ -1,17 +1,6 @@
-// const btn = document.querySelector("button"); // Get the button from the page
-// // Detect clicks on the button
-// if (btn) {
-//   btn.onclick = function() {
-//     // The JS works in conjunction with the 'dipped' code in style.css
-//     var number = Math.floor(Math.random() * 12);
-//     var gif = '<img src="https://cdn.glitch.global/c4be26a2-ff84-4e3e-a8ff-60d78823621a/' + number + '.gif"/>'
-//     document.getElementById("eeaao_GIF").innerHTML = gif;
-//   };
-// }
-
 function changeUni() {
   var number = Math.floor(Math.random() * 12);
-  var gif = '<img id="eeaao_img" onmouseover="changeUni()" src="https://cdn.glitch.global/c4be26a2-ff84-4e3e-a8ff-60d78823621a/' + number + '.gif"/>'
+  var gif = '<img id="eeaao_img" onmouseover="changeUni()" src="assets/' + number + '.gif"/>'
   document.getElementById("eeaao_GIF").innerHTML = gif;
 }
 
@@ -27,7 +16,7 @@ onclick = (event) => {
   var display_eyes = "";
   
   for (var i = 0; i < eyes.length; i++) {
-    display_eyes += '<img id="googlyEye" style="left: ' +  eyes[i][0] + 'px; top: ' +  eyes[i][1] + 'px;" src="https://cdn.glitch.global/c4be26a2-ff84-4e3e-a8ff-60d78823621a/icon.png?v=1708594443137"/>'
+    display_eyes += '<img id="googlyEye" style="left: ' +  eyes[i][0] + 'px; top: ' +  eyes[i][1] + 'px;" src="assets/icon.png"/>'
   }
   document.getElementById("googly_eyes").innerHTML = display_eyes;
 };
